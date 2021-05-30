7ecf9eb329cdd4bb859a96609a98ab0d0ab2c6fa
Robert Vanderwall <rvanderwall@yahoo.com>
2021-05-29 21:14:40 -0400

    Start adding display code

---
+++ samples/display.py
@@ -0,0 +1,82 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+Be sure to check the learn guides for more usage information.
+
+This example is for use on (Linux) computers that are using CPython with
+Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+not support PIL/pillow (python imaging library)!
+
+Author(s): Melissa LeBlanc-Williams for Adafruit Industries
+"""
+
+import digitalio
+import board
+from PIL import Image, ImageDraw
+import adafruit_rgb_display.ili9341 as ili9341
+#import adafruit_rgb_display.st7789 as st7789  # pylint: disable=unused-import
+#import adafruit_rgb_display.hx8357 as hx8357  # pylint: disable=unused-import
+#import adafruit_rgb_display.st7735 as st7735  # pylint: disable=unused-import
+#import adafruit_rgb_display.ssd1351 as ssd1351  # pylint: disable=unused-import
+#import adafruit_rgb_display.ssd1331 as ssd1331  # pylint: disable=unused-import
+
+# Configuration for CS and DC pins (these are PiTFT defaults):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = digitalio.DigitalInOut(board.D24)
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 24000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the display:
+disp = ili9341.ILI9341(
+    spi,
+    rotation=90,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+if disp.rotation % 180 == 90:
+    height = disp.width  # we swap height/width to rotate it to landscape!
+    width = disp.height
+else:
+    width = disp.width  # we swap height/width to rotate it to landscape!
+    height = disp.height
+image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image)
+
+image = Image.open("blinka.jpg")
+
+# Scale the image to the smaller screen dimension
+image_ratio = image.width / image.height
+screen_ratio = width / height
+if screen_ratio < image_ratio:
+    scaled_width = image.width * height // image.height
+    scaled_height = height
+else:
+    scaled_width = width
+    scaled_height = image.height * width // image.width
+image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+# Crop and center the image
+x = scaled_width // 2 - width // 2
+y = scaled_height // 2 - height // 2
+image = image.crop((x, y, x + width, y + height))
+
+# Display image.
+disp.image(image)
+
